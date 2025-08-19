32f8c84f0999b4bb924ee10230b8cfc34a4072b0
hyefyze <hafizmatqwer@gmail.com>
2025-08-19 15:40:58 +0800

    feat: Implement code changes to enhance functionality and improve performance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Card, Button, Checkbox } from 'flowbite-svelte';
+	import { Fuel, Cog, Car, Users, Circle, Droplet } from 'lucide-svelte';
 
 	const conditionColors: Record<string, string> = {
 		New: 'bg-green-100 text-green-800',
@@ -15,12 +16,15 @@
 			year: 2022,
 			model: 'G30 530e',
 			type: 'Sedan',
+			seats: 5,
 			price: 183000,
 			fuel: 'Hybrid',
+			fuel_consumption: '2.3l/100km',
 			mileage: '10-15K (KM)',
 			img: '/image/bmw-530e.jpg',
 			link: 'https://www.carlist.my/listings/details?id=17574198&controller=used-cars-for-sale-malaysia&slug=bmw-530e-2-0-a-m-sport-lci-original-facelift-full-service-record-12k-under-warranty-until-2027-battery-warranty-until-2030-local-bmw-unit',
 			drivetrain: 'RWD',
+			wheel_size: 19,
 			engine: { displacement: 2.0, cylinders: 4, hp: 248, nm: 350 }
 		},
 		{
@@ -30,12 +34,15 @@
 			year: 2025,
 			model: 'J7',
 			type: 'SUV',
+			seats: 5,
 			price: 138000,
 			fuel: 'Petrol',
+			fuel_consumption: '7.7l/100km',
 			mileage: '0 (KM)',
 			img: '/image/jaecoo-j7.jpg',
 			link: 'https://www.omodajaecoo.com.my/modelj7',
 			drivetrain: 'FWD',
+			wheel_size: 19,
 			engine: { displacement: 1.6, cylinders: 4, hp: 194, nm: 290 }
 		},
 		{
@@ -45,12 +52,15 @@
 			year: 2019,
 			model: 'G30 523i',
 			type: 'Wagon',
+			seats: 5,
 			price: 168000,
 			fuel: 'Petrol',
+			fuel_consumption: '5.8l/100km',
 			mileage: '30-35K(KM)',
 			img: '/image/bmw-523i.jpg',
 			link: 'https://www.carlist.my/listings/details?id=17773470&controller=recon-cars-for-sale-malaysia&slug=2019-bmw-523i-2-0-m-sport-full-spec-free-6-year-warranty',
 			drivetrain: 'FWD',
+			wheel_size: 19,
 			engine: { displacement: 2.0, cylinders: 4, hp: 250, nm: 290 }
 		},
 		{
@@ -60,12 +70,15 @@
 			year: 2025,
 			model: 'J7 PHEV',
 			type: 'SUV',
+			seats: 5,
 			price: 158000,
 			fuel: 'Hybrid',
+			fuel_consumption: '4.7l/100km',
 			mileage: '0 (KM)',
 			img: '/image/jaecoo-j7.jpg',
 			link: 'https://www.omodajaecoo.com.my/modelj7phev',
 			drivetrain: 'FWD',
+			wheel_size: 19,
 			engine: { displacement: 1.5, cylinders: 4, hp: 346, nm: 525 }
 		},
 		{
@@ -75,28 +88,52 @@
 			year: 2025,
 			model: 'C9',
 			type: 'SUV',
+			seats: 5,
 			price: 168000,
 			fuel: 'Petrol',
+			fuel_consumption: '8.2l/100km',
 			mileage: '0 (KM)',
 			img: '/image/omoda-c9.jpg',
 			link: 'https://www.omodajaecoo.com.my/modelc9',
 			drivetrain: 'FWD',
+			wheel_size: 20,
 			engine: { displacement: 2.0, cylinders: 4, hp: 257, nm: 400 }
 		},
-        {
+		{
 			id: 6,
 			condition: 'New',
 			brand: 'Proton',
 			year: 2025,
-			model: 'S70',
+			model: 'S70 Flagship',
 			type: 'Sedan',
+			seats: 5,
 			price: 89800,
 			fuel: 'Petrol',
+			fuel_consumption: '6.4l/100km',
 			mileage: '0 (KM)',
 			img: '/image/proton-s70.jpg',
 			link: 'https://www.proton.com/en/models/protons70',
 			drivetrain: 'FWD',
+			wheel_size: 17,
 			engine: { displacement: 1.5, cylinders: 3, hp: 150, nm: 226 }
+		},
+		{
+			id: 7,
+			condition: 'Recon',
+			brand: 'Mercedes',
+			year: 2020,
+			model: 'CLA200D',
+			type: 'Wagon',
+			seats: 5,
+			price: 175000,
+			fuel: 'Dieesel',
+			fuel_consumption: '4.9l/100km',
+			mileage: '35-40K (KM)',
+			img: '/image/mercedes-cla200d.jpg',
+			link: 'https://www.carlist.my/recon-cars/2020-mercedes-benz-cla200d-2-0-amg-shooting-brake-full-spec-free-6-year-warranty/16742616',
+			drivetrain: 'FWD',
+			wheel_size: 18,
+			engine: { displacement: 2.0, cylinders: 4, hp: 150, nm: 320 }
 		}
 	];
 	let sortOrder: 'high' | 'low' = 'high';
@@ -122,18 +159,14 @@
 
 <!-- Brand Filter Buttons -->
 <section class="flex flex-wrap gap-2 bg-gray-50 p-4">
-    <h2 class="w-full font-semibold">Brand Filter</h2>
+	<h2 class="w-full font-semibold">Brand Filter</h2>
 	{#each brands as brand}
 		<Button
 			color={selectedBrand === brand ? 'dark' : 'light'}
 			onclick={() => (selectedBrand = selectedBrand === brand ? null : brand)}
 			size="sm"
 		>
-			<img
-				src={`/image/${brand.toLowerCase()}.png`}
-				alt={brand}
-				class=" max-h-8 max-w-13"
-			/>
+			<img src={`/image/${brand.toLowerCase()}.png`} alt={brand} class=" max-h-8 max-w-13" />
 			<!-- {#if brand !== 'Jaecoo' && brand !== 'Omoda' && brand !== 'Proton'}
 				{brand}
 			{/if} -->
@@ -161,12 +194,20 @@
 
 			<!-- Car Info -->
 			<div class="flex min-w-0 flex-1 flex-col justify-between">
-				<!-- Price & Model -->
-				<div class="mb-2">
-					<h2 class="mb-1 truncate text-xl font-bold text-green-600">
+				<!-- Price & Mileage Row -->
+				<div class="mb-2 flex items-center justify-between">
+					<h2 class="truncate text-2xl font-bold text-green-600">
 						RM{car.price.toLocaleString()}
 					</h2>
-					<h3 class="truncate text-lg font-semibold">{car.brand} {car.model} ({car.year})</h3>
+					<p class="ml-auto pl-20 text-sm text-gray-500">Mileage: {car.mileage}</p>
+				</div>
+
+				<!-- Model & Condition -->
+				<div class="mb-2">
+					<h3 class="truncate text-xl font-semibold">
+						{car.brand}
+						{car.model} ({car.year})
+					</h3>
 					<p class="mt-1 text-sm">
 						Condition:
 						<span
@@ -176,18 +217,36 @@
 						</span>
 					</p>
 				</div>
-
 				<!-- Engine Info -->
-				<div class="mb-2">
+				<div class="mb-1">
 					<p class="text-sm font-semibold">Engine:</p>
 					<p class="pl-1 text-sm text-gray-700">
 						Fuel: {car.fuel} | {car.engine.displacement}cc, {car.engine.hp}HP, {car.engine.nm}Nm, {car
 							.engine.cylinders} Cylinders
 					</p>
 				</div>
-
-				<!-- Mileage -->
-				<p class="mb-2 text-sm text-gray-500">Mileage: {car.mileage}</p>
+				<hr />
+				<!-- General Info -->
+				<div class="space-y-1 my-2">
+					<p class="flex items-center text-sm text-gray-500">
+						<Fuel class="mr-2 h-4 w-4 text-gray-600" /> Fuel: {car.fuel}
+					</p>
+					<p class="flex items-center text-sm text-gray-500">
+						<Cog class="mr-2 h-4 w-4 text-gray-600" /> Drivetrain: {car.drivetrain}
+					</p>
+					<p class="flex items-center text-sm text-gray-500">
+						<Car class="mr-2 h-4 w-4 text-gray-600" /> Body Type: {car.type}
+					</p>
+					<p class="flex items-center text-sm text-gray-500">
+						<Users class="mr-2 h-4 w-4 text-gray-600" /> Seats: {car.seats}
+					</p>
+					<p class="flex items-center text-sm text-gray-500">
+						<Circle class="mr-2 h-4 w-4 text-gray-600" /> Wheel Size: {car.wheel_size} inch
+					</p>
+					<p class="flex items-center text-sm text-gray-500">
+						<Droplet class="mr-2 h-4 w-4 text-gray-600" /> Fuel consumption: {car.fuel_consumption}
+					</p>
+				</div>
 
 				<!-- Link -->
 				<a
